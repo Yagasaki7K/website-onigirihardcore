66f752dba01a41bc2ee146c0d073178ae3fa9234
Anderson Marlon <anderson18.marlon@gmail.com>
2022-07-21 19:09:09 -0300

    SEO + Slide fixed

--- src/components/LastNews.jsx
+++ src/components/LastNews.jsx
@@ -24,6 +24,17 @@ const LastNews = (props) => {
                                     {items?.description}
                                 </p>
                             </div>
+
+                            <meta name="description" content={items?.description} />
+                            <meta name="twitter:card" content="summary"/>
+                            <meta name="twitter:site" content="@publisher_handle"/>
+                            <meta name="twitter:title" content={items?.title}/>
+                            <meta name="twitter:description" content={items?.description}/>
+                            <meta name="twitter:creator" content="@Yagasaki7K"/>
+                            <meta name="twitter:image" content={getImages(items?.mainImage.asset._ref)}/>
+
+                            <meta property="og:title" content={items?.title} />
+                            <meta property="og:type" content="article" />
                         </div>
                     ))
                 }
