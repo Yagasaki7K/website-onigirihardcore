e4f49361ded862893f26cf42da8aea6b3c6b3ce3
Anderson Marlon <anderson18.marlon@gmail.com>
2022-10-20 21:31:16 -0300

    Change thumbnail on SEO

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -53,7 +53,7 @@ const Post = ({data}) => {
                                     <meta property="og:description" content={post.description} />
                                     <meta property="og:site_name" content="Anderson Marlon // Software Developer" />
                                     <meta property="og:url" content="https://yagasaki.vercel.app/" />
-                                    <meta property="og:image" content={thumbnail} />
+                                    <meta property="og:image" content={post.image} />
                                     <meta property="og:type" content="Website" />
                                     <meta property="og:image:width" content="1200" />
                                     <meta property="og:image:height" content="627" />
@@ -62,27 +62,27 @@ const Post = ({data}) => {
                                     <meta name="twitter:creator" content='Anderson "Yagasaki" Marlon' />
                                     <meta name="twitter:title" content={post.title} />
                                     <meta name="twitter:description" content={post.description} />
-                                    <meta name="twitter:image:src" content={thumbnail} />
+                                    <meta name="twitter:image:src" content={post.image} />
                                 </Head>
 
-                                <img src={post?.image}></img>
-                                <section key={post?.id}>
+                                <img src={post.image}></img>
+                                <section key={post.id}>
                                     <p className="block__content">{post.createdAtExtended} | {post.author}</p>
-                                    <h1 className="title__content">{post?.title}</h1>
-                                    <p className="block__content" style={{whiteSpace: "pre-wrap"}}>{post?.body}</p>
+                                    <h1 className="title__content">{post.title}</h1>
+                                    <p className="block__content" style={{whiteSpace: "pre-wrap"}}>{post.body}</p>
 
                                     {
                                         post.citation != '' ? (
-                                            <a href={post?.linkCitation} className="citation" target="_blank">
-                                                <p className="block__content">“{post?.citation}”</p>
+                                            <a href={post.linkCitation} className="citation" target="_blank">
+                                                <p className="block__content">“{post.citation}”</p>
                                             </a>
                                         ) : null
                                     }
 
-                                    <p className="block__content" style={{whiteSpace: "pre-wrap"}}>{post?.body2}</p>
+                                    <p className="block__content" style={{whiteSpace: "pre-wrap"}}>{post.body2}</p>
 
                                     {
-                                        post?.ytid ?
+                                        post.ytid ?
                                             <iframe width="550" height="480" src={'https://www.youtube.com/embed/' + post?.ytid} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> : null
                                     }
                                 </section>
