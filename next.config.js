277171468ca28e09e66bfe5a9e4e0f45e7cbdd94
Anderson Marlon <anderson18.marlon@gmail.com>
2022-10-20 21:19:27 -0300

    Change to SSG

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-},
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -5,9 +5,30 @@ import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
 import SlugDetails from '../src/components/SlugDetails'
 
-import posts from '../server/index.json'
+export async function getStaticProps() {
+    const DataAPI = await fetch('https://apionigirihardcore.up.railway.app/api/news')
+    const data = await DataAPI.json()
+    return {
+        props: {
+            data
+        }
+    }
+}
 
-const Post = () => {
+export async function getStaticPaths() {
+    const request = await fetch('https://apionigirihardcore.up.railway.app/api/news')
+    const posts = await request.json()
+    const paths = posts.map(client => ({
+        params: { slug: client.slug.toString() },
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+const Post = ({data}) => {
     const router = useRouter()
     const { slug } = router.query
     return (
@@ -16,9 +37,26 @@ const Post = () => {
 
             <SlugDetails>
                 {
-                    posts && posts.map(post => (
+                    data && data.map(post => (
                         post.slug === slug ? (
                             <>
+                                <Head>
+                                    <title>{post?.title}</title>
+                                    <meta name="title" content={post?.title} />
+                                    <meta name="author" content="Anderson 'Yagasaki' Marlon" />
+
+                                    <meta name="description" content={post.description} />
+                                    <meta property="og:url" content={'https://onigirihardcore.vercel.app/' + post.slug} />
+                                    <meta property="og:title" content={post.title} />
+                                    <meta property="og:description" content={post.description} />
+                                    <meta property="og:image" content={post.image} />
+
+                                    <meta property="twitter:url" content={'https://onigirihardcore.vercel.app/' + post.slug}/>
+                                    <meta property="twitter:title" content={post.title} />
+                                    <meta property="twitter:description" content={post.description} />
+                                    <meta property="twitter:image" content={post.image} />
+                                </Head>
+                                
                                 <img src={post?.image}></img>
                                 <section key={post?.id}>
                                     <p className="block__content">{post.createdAtExtended} | {post.author}</p>
@@ -40,23 +78,6 @@ const Post = () => {
                                             <iframe width="550" height="480" src={'https://www.youtube.com/embed/' + post?.ytid} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> : null
                                     }
                                 </section>
-
-                                <Head>
-                                    <title>{post?.title}</title>
-                                    <meta name="title" content={post?.title} />
-                                    <meta name="author" content="Anderson 'Yagasaki' Marlon" />
-
-                                    <meta name="description" content={post.description} />
-                                    <meta property="og:url" content={'https://onigirihardcore.vercel.app/' + post.slug} />
-                                    <meta property="og:title" content={post.title} />
-                                    <meta property="og:description" content={post.description} />
-                                    <meta property="og:image" content={post.image} />
-
-                                    <meta property="twitter:url" content={'https://onigirihardcore.vercel.app/' + post.slug}/>
-                                    <meta property="twitter:title" content={post.title} />
-                                    <meta property="twitter:description" content={post.description} />
-                                    <meta property="twitter:image" content={post.image} />
-                                </Head>
                             </>
                         ) : (
                             null
