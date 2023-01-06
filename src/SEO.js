27f5f752b83e9618e4fa5f27c7b9908be4686af4
Anderson Marlon <anderson18.marlon@gmail.com>
2023-01-05 22:22:06 -0300

    Post: Velozes + The Last

--- src/SEO.js
+++ src/SEO.js
@@ -3,8 +3,6 @@ const SEO = {
     description: 'Um site nerd entregando conteúdos nerds desde 2013',
     siteThumbnail: 'https://i.imgur.com/VoOogmx.jpg',
     siteUrl: "https://onigirihardcore.vercel.app",
-    siteLogo: "https://example.com/images/logo.png",
-    siteLogoSquare: "https://example.com/images/logox200.png",
     email: "kalifyinc@gmail.com",
     twitter: "https://twitter.com/OnigiriHardcore",
     twitterHandle: "@OnigiriHardcore",
