8c8ae72104215fdbef449eceb4805d845adfeaad
Anderson Marlon <anderson18.marlon@gmail.com>
2023-04-18 14:00:58 -0300

    [SEO] Added Logo + _documents

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -37,8 +37,36 @@ export default class MyDocument extends Document {
                 <title>Onigiri Hardcore | Portal de Notícias e Entretenimento</title>
                 <Head>
                     {/* PWA primary color */}
-                    <link rel="shortcut icon" href="/icone.png" type="image/png"></link>
+                    <link rel="shortcut icon" type="image/png" href="/logo.png" />
+                    <link rel="apple-touch-icon" href="/logo.png" />
+                    <meta name="application-name" content="Onigiri Hardcore | Portal de Notícias e Entretenimento" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                    <meta name="apple-mobile-web-app-title" content="Essentials" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta name="theme-color" content='#00acff' />
+
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
+
+                    <meta name="title" content="Onigiri Hardcore | Portal de Notícias e Entretenimento" />
+                    <meta name="keywords" content="anime, noticia, filme, serie, entretenimento, onigiri, hardcore, nerd, geek" />
+                    <meta name="robots" content="index, follow" />
+                    <meta name="author" content="Anderson 'Yagasaki' Marlon" />
+
+                    <meta name="description" content="Um site nerd com conteúdos nerds" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://onigirihardcore.vercel.com/" />
+                    <meta property="og:title" content="Onigiri Hardcore | Portal de Notícias e Entretenimento" />
+                    <meta property="og:description" content="Um site nerd com conteúdos nerds" />
+                    <meta property="og:image" content="https://i.imgur.com/VoOogmx.png" />
+
+                    <meta property="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:url" content="https://onigirihardcore.vercel.com/" />
+                    <meta property="twitter:title" content="Onigiri Hardcore | Portal de Notícias e Entretenimento" />
+                    <meta property="twitter:description" content="Um site nerd com conteúdos nerds" />
+                    <meta property="twitter:image" content="https://i.imgur.com/VoOogmx.png" />
+
                 </Head>
                 <body>
                     <Main />
