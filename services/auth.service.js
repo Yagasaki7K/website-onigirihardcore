d8e9642ecfab0ec87a6c8b318482b99da4662484
gabrielUpON <gabrielbsdeveloper@gmail.com>
2023-04-24 16:47:43 -0300

    [Login/EditDelete] Update Auth Service

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,12 +1,16 @@
 import {
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from "firebase/auth";
 import auth from "../client";
 
 class usersService {
     usersSignOutWithEmailPassword = async (email, password) => {
-        return await createUserWithEmailAndPassword(auth, )
+        return await createUserWithEmailAndPassword(auth, email, password)
     }
 
+    usersSignInWithEmailAndPassword = async (email, password) => {
+        return await signInWithEmailAndPassword(auth, email, password)
+    }
 }
 export default new usersService();
