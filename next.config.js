1836b9470f01a51ca55df18613d4fc1261559f4b
Anderson Marlon <anderson18.marlon@gmail.com>
2023-04-25 20:20:48 -0300

    [Fix] Issue https://github.com/Yagasaki7K/website-onigirihardcore/commit/0d0b24b86046eaf05bc7f9bf41ade3de2f4a6c25

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /* eslint-disable no-undef */
-
 const removeImports = require("next-remove-imports")();
+
 module.exports = removeImports({
-    env: {
-        FIREBASE_API_KEY: "AIzaSyB305sY0GR-z1lnJx2lqnG0kkAr-JR-IeQ",
-        FIREBASE_AUTH_DOMAIN: "kalify-findyourpet.firebaseapp.com",
-        FIREBASE_DATABASE_URL:
-            "https://kalify-findyourpet-default-rtdb.firebaseio.com",
-        FIREBASE_PROJECT_ID: "kalify-findyourpet",
-        FIREBASE_STORAGE_BUCKET: "kalify-findyourpet.appspot.com",
-        FIREBASE_MESSAGING_SENDER_ID: "843679913436",
-        FIREBASE_APP_ID: "1:843679913436:web:55e00dc38e1731adbb6b19",
-    },
-    webpack(config) {
+    webpack: (config) => {
         return config;
     },
 });
