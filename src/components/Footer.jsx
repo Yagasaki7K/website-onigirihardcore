1ce190834b7322a08071610c6096071b2f99eae4
Anderson Marlon <anderson18.marlon@gmail.com>
2023-05-02 11:14:41 -0300

    [Footer] Fixed Spaces

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,8 @@ export default function Footer() {
 
             <div className="footer">
                 <p>
-                    <a href="/login">©</a> 2015 - {new Date().getFullYear()} <a href="/">Onigiri Hardcore</a> - &nbsp;
-                    <a href="https://kalifyinc.vercel.app/" target="_blank" rel="noreferrer">Kalify Inc.</a> | &nbsp;
+                    <a href="/login">©</a> 2015 - {new Date().getFullYear()} <a href="/">Onigiri Hardcore</a> -&nbsp;
+                    <a href="https://kalifyinc.vercel.app/" target="_blank" rel="noreferrer">Kalify Inc.</a> |&nbsp;
                 </p>
                 <p>
                     Todas as imagens de filmes, séries e etc são marcas registradas dos seus respectivos proprietários
