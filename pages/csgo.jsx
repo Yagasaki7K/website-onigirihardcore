7b13e79582ea80f88cc82991b7136c938538d1e5
Anderson Marlon <anderson18.marlon@gmail.com>
2023-05-15 09:41:25 -0300

    [SEO] Slug page using next-seo config #3

--- pages/csgo.jsx
+++ pages/csgo.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Header from '../src/components/Header'
 import "keen-slider/keen-slider.min.css"
 import CSGODetails from '../src/components/CSGODetails'
+import { NextSeo } from 'next-seo'
 
 export async function getStaticProps() {
     const news = await HLTV.getNews()
@@ -22,8 +23,32 @@ export async function getStaticProps() {
 const csgo = ({ data }) => {
     return (
         <>
+            <NextSeo
+                title='Onigiri Hardcore | Portal de Notícias e Entretenimento'
+                description='Um site nerd com conteúdos nerds'
+                canonical={`https://onigirihardcore.com.br/`}
+                openGraph={{
+                    url: 'https://onigirihardcore.com.br/',
+                    title: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                    description: 'Um site nerd com conteúdos nerds',
+                    images: [
+                        {
+                            url: 'https://i.imgur.com/VoOogmx.png',
+                            width: 460,
+                            height: 460,
+                            alt: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                            type: 'image/jpeg' || 'image/png',
+                        }
+                    ],
+                    siteName: 'Onigiri Hardcore',
+                }}
+                twitter={{
+                    handle: '@OnigiriHardcore',
+                    site: '@OnigiriHardcore',
+                    cardType: 'summary_large_image',
+                }}
+            />
             <Header />
-
             <CSGODetails>
                 <div className="news">
                     <h1>Últimas notícias (via HLTV)</h1>
--- pages/noticias.jsx
+++ pages/noticias.jsx
@@ -3,6 +3,7 @@ import Footer from '../src/components/Footer'
 import Header from '../src/components/Header'
 import FullNewsDetails from '../src/components/FullNewsDetails'
 import postService from '../services/post.service'
+import { NextSeo } from 'next-seo'
 
 // import post from '../server/index.json'
 
@@ -22,6 +23,31 @@ const FullNews = () => {
 
     return (
         <>
+            <NextSeo
+                title='Onigiri Hardcore | Portal de Notícias e Entretenimento'
+                description='Um site nerd com conteúdos nerds'
+                canonical={`https://onigirihardcore.com.br/`}
+                openGraph={{
+                    url: 'https://onigirihardcore.com.br/',
+                    title: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                    description: 'Um site nerd com conteúdos nerds',
+                    images: [
+                        {
+                            url: 'https://i.imgur.com/VoOogmx.png',
+                            width: 460,
+                            height: 460,
+                            alt: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                            type: 'image/jpeg' || 'image/png',
+                        }
+                    ],
+                    siteName: 'Onigiri Hardcore',
+                }}
+                twitter={{
+                    handle: '@OnigiriHardcore',
+                    site: '@OnigiriHardcore',
+                    cardType: 'summary_large_image',
+                }}
+            />
             <Header />
             <FullNewsDetails>
                 <div className="fullnews">
