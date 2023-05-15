aab65f65cbdeb775fcc99b646f34f868506e160d
Anderson Marlon <anderson18.marlon@gmail.com>
2023-05-15 09:16:02 -0300

    [SEO] Slug page using next-seo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,6 @@
 import { ServerStyleSheet } from 'styled-components';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { NextSeo } from 'next-seo';
 
 export default class MyDocument extends Document {
 
@@ -34,21 +35,12 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="pt-br">
-                <title>Onigiri Hardcore | Portal de Notícias e Entretenimento</title>
                 <Head>
                     <link rel="shortcut icon" type="image/png" href="/logo.png" />
                     <link rel="apple-touch-icon" href="/logo.png" />
-
-                    <meta name="application-name" content="Onigiri Hardcore | Portal de Notícias e Entretenimento" />
-                    <meta name="description" content="Um site nerd com conteúdos nerds" />
                     <meta name="keywords" content="anime, noticia, filme, serie, entretenimento, onigiri, hardcore, nerd, geek" />
                     <meta name="author" content="Anderson 'Yagasaki' Marlon" />
                     <meta name="robots" content="index, follow" />
-
-                    <meta name="theme-color" content='#00acff' />
-                    <meta name="apple-mobile-web-app-capable" content="yes" />
-                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                    <meta name="apple-mobile-web-app-title" content="Essentials" />
 
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
@@ -68,6 +60,31 @@ export default class MyDocument extends Document {
                     <meta property="twitter:description" content="Um site nerd com conteúdos nerds" />
                     <meta property="twitter:image" content="https://i.imgur.com/VoOogmx.png" /> */}
                 </Head>
+                <NextSeo
+                    title='Onigiri Hardcore | Portal de Notícias e Entretenimento'
+                    description='Um site nerd com conteúdos nerds'
+                    canonical={`https://onigirihardcore.com.br/`}
+                    openGraph={{
+                        url: 'https://onigirihardcore.com.br/',
+                        title: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                        description: 'Um site nerd com conteúdos nerds',
+                        images: [
+                            {
+                                url: 'https://i.imgur.com/VoOogmx.png',
+                                width: 460,
+                                height: 460,
+                                alt: 'Onigiri Hardcore | Portal de Notícias e Entretenimento',
+                                type: 'image/jpeg' || 'image/png',
+                            }
+                        ],
+                        siteName: 'Onigiri Hardcore',
+                    }}
+                    twitter={{
+                        handle: '@OnigiriHardcore',
+                        site: '@OnigiriHardcore',
+                        cardType: 'summary_large_image',
+                    }}
+                />
                 <body>
                     <Main />
                     <NextScript />
