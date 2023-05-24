e8676fd688e2293efd0e7a8acd7c83f371263074
Gabriel Barbosa <61061170+gabrielUpON@users.noreply.github.com>
2023-05-24 16:16:14 +0000

    [Crud/Auth] getDoc Fix in Modal

--- pages/login/posts.jsx
+++ pages/login/posts.jsx
@@ -8,7 +8,7 @@ import DashboardDetails from "../../src/components/DashboardDetails"
 //import SideMenuDetails from '../../src/components/Login/SideMenuDetails'
 import SideMenu from '../../src/components/Login/SideMenu'
 //import LoginDetails from "../../src/components/LoginDetails"
-import EditPostModal from "../../src/components/Modals/EditPostModal"
+import { EditPostModal } from "../../src/components/Modals/EditPostModal"
 
 const Login = () => {
     const [Posts, setPosts] = useState([])
@@ -49,7 +49,7 @@ const Login = () => {
                                         <td>{post.author}</td>
                                         <td>{post.categories}</td>
                                         <td>{post.lessDate}</td>
-                                        <td><EditPostModal /></td>
+                                        <td><EditPostModal param={post.id}/></td>
                                         <td><button className="deletar" onClick={() => handleDelete(post.id, post.image)}>Deletar</button></td>
                                     </tr>
                                 ))
--- src/components/Modals/EditPostModal.jsx
+++ src/components/Modals/EditPostModal.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 import ModalDetails from './ModalDetails';
+import postService from '../../../services/post.service';
 
-const EditPostModal = () => {
-    const [modalOpen, setModalOpen] = useState(false)
+export function EditPostModal(id) {
+    const [modalOpen, setModalOpen] = useState(false);
 
-    function handleOpenModal() {setModalOpen(true)}
+    
+    async function handleOpenModal() {
+        setModalOpen(true)
+
+        const data = await postService.getPost(id.param)
+        const fields = data.data(document)
+
+        const inputTitle = document.getElementById('title')
+        inputTitle.value = fields.title
+
+        const inputBodyPost = document.getElementById('bodyPost')
+        inputBodyPost.value = fields.bodyPost
+
+        const inputYtid = document.getElementById('ytid')
+        inputYtid.value = fields.ytid
+
+        const inputLinkCitation = document.getElementById('linkCitation')
+        inputLinkCitation.value = fields.linkCitation
+
+    }
 
     function handleCloseModal() {setModalOpen(false)}
 
@@ -18,8 +38,27 @@ const EditPostModal = () => {
                         <div className="publi" id="publi">
                             <h1>Editar Publicação</h1>
                             <div className="form-group">
-                                <form>
-                                    Em construção...
+                                <form>                                
+                                    <div className='item'>
+                                        <label htmlFor="password">Titulo </label>
+                                        <input type="text" name="text" id="title" value=""/>
+                                    </div>
+
+                                    <div className='item'>
+                                        <label htmlFor="password">Corpo: </label>
+                                        <input type="text" name="text" id="bodyPost" value=""/>
+                                    </div>
+
+                                    <div className='item'>
+                                        <label htmlFor="password">Link Youtube: </label>
+                                        <input type="text" name="text" id="ytid" value=""/>
+                                    </div>
+
+                                    <div className='item'>
+                                        <label htmlFor="password">Link Twitter: </label>
+                                        <input type="text" name="password" id="linkCitation" value=""/>
+                                    </div>
+                                    <button onClick={handleCloseModal} className='closebtn'>Fechar</button>
                                 </form>
                             </div>
                         </div>
@@ -29,5 +68,3 @@ const EditPostModal = () => {
         </div>
     )
 }
-
-export default EditPostModal
