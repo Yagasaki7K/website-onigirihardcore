265cfc52c4fefade5e5560d46026330ade64d310
gabrielUpON <gabrielbsdeveloper@gmail.com>
2023-06-16 08:40:20 -0300

    [Crud/Auth] Page 404 created

---
+++ src/components/Page404Details.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { colors } from "./variables";
+
+const Page404Details = styled.div`
+    .error_description {
+        text-align: center;
+        
+        h1 {
+            color: ${colors.whitelight};
+        }
+
+    }
+`;
+export default Page404Details;
