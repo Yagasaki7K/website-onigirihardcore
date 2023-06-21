0169f4b41cb17503f1bb86c65a7a4db89ee0456b
gabrielUpON <gabrielbsdeveloper@gmail.com>
2023-06-21 15:24:22 -0300

    [Crud/Auth] Update validation admin

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import authService from "../../services/auth.service";
+
+function Dashboard() {
+
+    authService.queryByAdmin("ug4AueY54Kh6mU7wQRnU1jp0HN92")
+
+    return (
+        <>
+            <p>oi</p>
+        </>
+    )
+}
+
+export default Dashboard;
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,7 +1,21 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../client";
+import { query, where, collection, getDocs } from "firebase/firestore";
+import { auth, database } from "../client";
+
+const permissionsCollectionRef = collection(database, "permissions");
 
 class authService {
+    queryByAdmin = async (uid) => {
+        const result = await getDocs(
+            query(permissionsCollectionRef, where("user_uid", "==", uid))
+        );
+        result.forEach((doc) => {
+            doc.id === "admin"
+                ? console.log("Usuario pertence a permissão Admin")
+                : null;
+        });
+    };
+
     signInGoogle = async (router) => {
         const provider = new GoogleAuthProvider();
         return await signInWithPopup(auth, provider).then((result) => {
