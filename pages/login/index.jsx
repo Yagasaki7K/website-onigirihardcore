e7a1880077d174278e15e4e3f9b9a537760eb956
gabrielUpON <gabrielbsdeveloper@gmail.com>
2023-07-04 09:51:06 -0300

    [Crud/Auth] Added Link in Onigiri Logo

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import LoginDetails from "../../src/components/LoginDetails";
 import SignUpModal from "../../src/components/Modals/SignUpModal";
 import SignInModal from "../../src/components/Modals/SignInModal";
@@ -22,7 +23,10 @@ export default function Login() {
         <LoginDetails>
             <div className="container">
                 <div>
-                    <img src="/logotipo-white.png" alt="Logo" />
+                    <Link href="/">
+                        <img src="/logotipo-white.png" alt="Logo" />
+                    </Link>
+                    
                     <SignUpModal />
                     <SignInModal />
                 </div>
