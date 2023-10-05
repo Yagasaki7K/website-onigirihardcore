81457accd294fbc39f551a42f0311102ce929b56
Gabriel Santiago <61061170+gabrielUpON@users.noreply.github.com>
2023-10-05 01:37:27 +0000

    Update modal EditPostModal.jsx

--- src/components/Modals/EditPostModal.jsx
+++ src/components/Modals/EditPostModal.jsx
@@ -1,44 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import ModalDetails from "./ModalDetails";
 import dynamic from "next/dynamic";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
+import postService from "../../../services/post.service";
 
 const MarkdownEditor = dynamic(
     () => import("@uiw/react-md-editor").then((mod) => mod.default),
     { ssr: false }
 );
 
-export default function EditPostModal() {
-    const [fields, setFields] = useState({
-        date: "",
-        lessDate: "",
-        moreDate: "",
-        slug: "",
-        author: "",
-        title: "",
-        description: "",
-        categories: "",
-        bodyPost: "",
-        image: "",
-        imageUrl: "",
-        citation: "",
-        linkCitation: "",
-        ytid: "",
-    });
+export default function EditPostModal(param) {
+    const [documentPost, setDocumentPost] = useState([])
     const [modalOpen, setModalOpen] = useState(false);
 
-    async function handleOpenModal() {
-        setModalOpen(true);
+    useEffect(() => {
+        getPost()
+    }, [])
+
+    async function getPost() {
+        let data = await postService.getPost(param.id)
+        if (data.exists()) {
+            return setDocumentPost(data.data())
+        }
+    }
+
+    function handleOpenModal() {
+        return setModalOpen(true);
     }
 
     function handleCloseModal() {
-        setModalOpen(false);
+        return setModalOpen(false);
     }
 
     function updatePost() {
-        console.log("Deve ser atualizado");
+        return console.log("Deve ser atualizado");
     }
 
     return (
@@ -54,60 +51,54 @@ export default function EditPostModal() {
                             <div className="form-group">
                                 <form>
                                     <div className="item">
-                                        <label htmlFor="password">
-                                            Titulo{" "}
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="text"
-                                            id="title"
-                                            value="."
-                                        />
+                                        <label htmlFor="author">Autor*: </label>
+                                        <select id="author" defaultValue={documentPost.author}>
+                                            <option value={"Anderson 'Yagasaki' Marlon"}> Anderson Marlon </option>
+                                        </select>
+                                    </div>
+
+                                    <div className="item">
+                                        <label htmlFor="title"> Titulo{" "}</label>
+                                        <input type="text" name="text"id="title" defaultValue={documentPost.title} />
+                                    </div>
+
+                                    <div className="item">
+                                        <label htmlFor="description">Descrição da Publicação*:{" "}</label>
+                                        <textarea name="description" id="description" defaultValue={documentPost.description} maxLength={195} placeholder="Descrição da Publicação (Max.126)" required />
+                                    </div>
+
+                                    <div className="item">
+                                        <label htmlFor="categories">Categoria da Publicação*:{" "}</label>
+                                        <select id="categories" defaultValue={documentPost.categories}>
+                                            <option>Animes</option>
+                                            <option>Games</option>
+                                            <option>Movies</option>
+                                            <option>Technologies</option>
+                                            <option>Development</option>
+                                        </select>
                                     </div>
 
                                     <div className="item-markdown">
-                                        <label htmlFor="body">
-                                            Conteúdo da Publicação*:{" "}
-                                        </label>
-                                        <MarkdownEditor
-                                            height={300}
-                                            value={fields.bodyPost}
-                                            onChange={setFields.bodyPost}
-                                        />
+                                        <label htmlFor="body">Conteúdo da Publicação*:{" "}</label>
+                                        <MarkdownEditor height={300} />
                                     </div>
 
                                     <div className="item">
-                                        <label htmlFor="password">
-                                            Link Youtube:{" "}
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="text"
-                                            id="ytid"
-                                            value="."
-                                        />
+                                        <label htmlFor="password"> Link Youtube:{" "}</label>
+                                        <input type="text" name="text" id="ytid" defaultValue={documentPost.ytid} />
                                     </div>
 
                                     <div className="item">
-                                        <label htmlFor="password">
-                                            Link Twitter:{" "}
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="password"
-                                            id="linkCitation"
-                                            value="."
-                                        />
+                                        <label htmlFor="citation">Citação de Twitter:{" "}</label>
+                                        <textarea name="citation" id="citation" defaultValue={documentPost.citation} placeholder="Uma citação que foi encontrada no Twitter ou em outra fonte" />
                                     </div>
-                                    <button
-                                        onClick={handleCloseModal}
-                                        className="closebtn"
-                                    >
-                                        Fechar
-                                    </button>
-                                    <button onClick={updatePost}>
-                                        Atualizar
-                                    </button>
+
+                                    <div className="item">
+                                        <label htmlFor="linkcitation">Link da Citação:{" "}</label>
+                                        <input name="linkcitation" id="linkcitation" defaultValue={documentPost.linkCitation} placeholder="https://twitter.com/Yagasaki7K/example" />
+                                    </div>
+                                    <button onClick={handleCloseModal} className="closebtn">Fechar</button>
+                                    <button onClick={updatePost}>Atualizar</button>
                                 </form>
                             </div>
                         </div>
