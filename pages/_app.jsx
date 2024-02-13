d6116e2cbdc6712d6b7429cbdac43d0191a92e0c
Anderson Marlon <anderson18.marlon@gmail.com>
2024-02-12 22:50:24 -0300

    Added: Unable to copy, paste and inspect

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/globals.css'
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        const preventRightClick = (e) => {
+            e.preventDefault();
+        };
+
+        const preventCopyPaste = (e) => {
+            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86)) {
+                e.preventDefault();
+            }
+        };
+
+        const disableTextSelection = () => {
+            document.body.style.userSelect = 'none';
+        };
+
+        document.addEventListener('contextmenu', preventRightClick);
+        document.addEventListener('keydown', preventCopyPaste);
+        disableTextSelection();
+
+        return () => {
+            document.removeEventListener('contextmenu', preventRightClick);
+            document.removeEventListener('keydown', preventCopyPaste);
+            document.body.style.userSelect = '';
+        };
+    }, []);
+
     return <Component {...pageProps} />
 }
 
