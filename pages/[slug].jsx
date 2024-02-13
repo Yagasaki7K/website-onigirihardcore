84a741585f86876ec23405db516cb7c5546e7dfa
Anderson Marlon <anderson18.marlon@gmail.com>
2024-02-13 11:06:43 -0300

    Changed: useEffect against copy and paste to only Slug

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -22,33 +22,31 @@ const Post = () => {
     const [Post, setPost] = useState([])
 
     useEffect(() => {
-        useEffect(() => {
-            const preventRightClick = (e) => {
-                e.preventDefault();
-            };
-
-            const preventCopyPaste = (e) => {
-                if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86)) {
-                    e.preventDefault();
-                }
-            };
-
-            const disableTextSelection = () => {
-                document.body.style.userSelect = 'none';
-            };
-
-            document.addEventListener('contextmenu', preventRightClick);
-            document.addEventListener('keydown', preventCopyPaste);
-            disableTextSelection();
-
-            return () => {
-                document.removeEventListener('contextmenu', preventRightClick);
-                document.removeEventListener('keydown', preventCopyPaste);
-                document.body.style.userSelect = '';
-            };
-        }, []);
-
         getPost()
+
+        const preventRightClick = (e) => {
+            e.preventDefault();
+        };
+
+        const preventCopyPaste = (e) => {
+            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86)) {
+                e.preventDefault();
+            }
+        };
+
+        const disableTextSelection = () => {
+            document.body.style.userSelect = 'none';
+        };
+
+        document.addEventListener('contextmenu', preventRightClick);
+        document.addEventListener('keydown', preventCopyPaste);
+        disableTextSelection();
+
+        return () => {
+            document.removeEventListener('contextmenu', preventRightClick);
+            document.removeEventListener('keydown', preventCopyPaste);
+            document.body.style.userSelect = '';
+        };
     }, [])
 
     const getPost = async () => {
