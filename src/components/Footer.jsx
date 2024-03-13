527046d6b7d25443d6583fff86d6970645e297cc
Anderson Marlon <anderson18.marlon@gmail.com>
2024-03-13 16:18:20 -0300

    Fix: Copyright on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
 
             <div className="footer">
                 <p>
-                    <a href="/login">Copyright ©</a> 2013 - {new Date().getFullYear()} <a href="/">Onigiri Hardcore</a> by&nbsp;
+                    <a href="/login">©</a> 2013 - {new Date().getFullYear()} <a href="/">Onigiri Hardcore</a> by&nbsp;
                     <a href="https://kalify.vercel.app/" target="_blank" rel="noreferrer">Kalify Inc.</a> |&nbsp;Todas as imagens são marcas registradas dos seus respectivos proprietários
                 </p>
             </div>
