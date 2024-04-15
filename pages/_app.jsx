2b1bf091f485afc0bbec27b2726189d432f490db
Anderson Marlon <anderson18.marlon@gmail.com>
2024-04-15 18:08:29 -0300

    Fix: Toaster - 15-04 18:08

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/globals.css'
+import { Toaster } from 'sonner'
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Toaster richColors position="top-right" />
+        </>
+    )
 }
 
 export default MyApp
