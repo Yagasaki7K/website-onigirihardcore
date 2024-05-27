16cbd1da69c46a3944dfe1e4b7d307df39ccf998
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-27 13:07:19 -0300

    Update: Modules + next.config + Added: New Posts - 27-05 13:07

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import million from 'million/compiler';
 
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    }
 }
 
 // eslint-disable-next-line no-undef
